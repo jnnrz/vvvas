044ece2441dc1b0f086e05828ff894666115c5a6
Johann Perozo <johann.prz@gmail.com>
2021-04-28 18:42:24 -0400

    Adds new field

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -11,5 +11,7 @@ namespace vvvas
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+
+	public string Something { get; set; }
     }
 }
