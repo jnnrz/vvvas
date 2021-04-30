d032f0007e20adef3e5e3dbed8469e6cd88576b8
Johann Perozo <johann.prz@gmail.com>
2021-04-30 03:56:00 -0400

    Adds Time controller

---
+++ Controllers/TimeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace vvvas.Controllers
+{
+    [ApiController]
+    public class TimeController : Controller
+    {
+        [HttpGet("/time")]
+        public async Task<IActionResult> Time()
+        {
+            var time = new
+            {
+                Time = DateTime.Now.ToString(CultureInfo.CurrentCulture)
+            };
+            
+            return await Task.Run(() => Ok(time));
+        }
+    }
+}
