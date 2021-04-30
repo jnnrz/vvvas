c41adbbcb778cd6cd9145267b3cc5b540010fea0
Johann Perozo <johann.prz@gmail.com>
2021-04-30 04:57:11 -0400

    Adds Note Controller

---
+++ Controllers/NoteController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vvvas.Models;
+
+namespace vvvas.Controllers
+{
+    [ApiController]
+    public class NoteController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Create([FromServices] AZContext context, [FromBody] Note note)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await context.Notes.AddAsync(note);
+
+                return Ok(note);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return Problem($"There was an error: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        public async Task<ICollection<Note>> GetNotes([FromServices] AZContext context)
+        {
+            return await context.Notes.ToListAsync();
+        }
+    }
+}
